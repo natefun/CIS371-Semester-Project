59babeb501ad45254ea389afbd34a41333bf924e
AaronZhao <valentinozhao@gmaol.com>
2021-04-15 13:40:14 -0400

    movieInfo component bug fixed

--- src/components/MovieInfo.vue
+++ src/components/MovieInfo.vue
@@ -2,25 +2,44 @@
   <section>
     <h1>Search a Movie</h1>
     Search By Movie Name <br /><input type="text" v-model="movieName" /><br />
-    <button @click="searchMovie">Go</button>
+    <button @click="getImage">Go</button>
     <button @click="goback">GoBack</button>
     <section>
-    <h2>Name: {{movieName}}</h2>
-    <h2>Year:{{year}}</h2>
-    <img :src="pic" style="width:100%;" alt="">
+      <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Year</th>
+          <th>Poster</th>
+        </tr>
+      </thead>
+      <tr v-for="(x, pos) in allMovies" :key="pos">
+        <td>{{ x.title }}</td>
+        <td>{{ x.year }}</td>
+        <td>{{ x.poster }}</td>
+      </tr>
+      <tbody></tbody>
+    </table>
     </section>
   </section>
 </template>
 <script lang="ts">
 import axios, { AxiosResponse } from "axios";
 import { Component, Vue } from "vue-property-decorator";
+
+interface Movie {
+  title: string;
+  year: number;
+  imdb_id: string;
+  poster: string;
+}
 @Component
 export default class MovieInfo extends Vue {
-  private movieName = "";
+  private movieName = "The Avengers";
   private id = "";
-  private pic = "";
+  //private pic: string[] = "";
   private year = 0;
-
+  private allMovies: Movie[] = [];
   goback() {
     this.$router.back();
   }
@@ -40,34 +59,28 @@ export default class MovieInfo extends Vue {
       .then((r: AxiosResponse) => r.data)
       .then((p: any) => p.movie_results)
       .then((movie: any[]) => {
-        this.id = movie[0].imdb_id;
-        this.year = movie[0].year;
-        console.log(this.id);
+        console.log(movie);
+        this.allMovies = movie;
+        for (let i = 0; i < movie.length; i++) {
+          //get image by id
+          console.log("movie[i]",movie[i])
+          console.log("allMovies",this.allMovies)
+          console.log("id",movie[i].imdb_id);
+          this.getImage(movie[i].imdb_id)
+          //this.allMovies.poster = poster
+          .then((po:string) => {
+              this.allMovies[i].poster = po;
+          })
+        }
       });
   }
-  searchMovie() {
-      axios
+  async getImage(id: string) {
+    return await axios
       .get("https://movies-tvshows-data-imdb.p.rapidapi.com/", {
         params: {
-          type: "get-movies-by-title",
-          title: this.movieName,
+          type: "get-movies-images-by-imdb",
+          imdb: id,
         },
-        headers: {
-          "x-rapidapi-key":
-            "ae7d7525edmshe28bd6a6eef3638p14f263jsnb3486bf90e1b",
-          "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
-        },
-      })
-      .then((r: AxiosResponse) => r.data)
-      .then((p: any) => p.movie_results)
-      .then((movie: any[]) => {
-        this.id = movie[0].imdb_id;
-        this.year = movie[0].year;
-        console.log(this.id);
-      });
-    axios
-      .get("https://movies-tvshows-data-imdb.p.rapidapi.com/", {
-        params: { type: "get-movies-images-by-imdb", imdb: this.id },
         headers: {
           "x-rapidapi-key":
             "ae7d7525edmshe28bd6a6eef3638p14f263jsnb3486bf90e1b",
@@ -76,9 +89,8 @@ export default class MovieInfo extends Vue {
       })
       .then((r: AxiosResponse) => r.data)
       .then((p: any) => {
-        this.pic = p.poster;
-        console.log(this.pic);
-      });
+        console.log("P in getImage", p)
+        return p.poster})
   }
 }
 </script>
