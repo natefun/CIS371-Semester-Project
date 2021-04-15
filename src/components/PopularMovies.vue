5d9c1bc651f6a33094ec2ee7222dd90f808b501e
AaronZhao <valentinozhao@gmaol.com>
2021-04-15 12:24:36 -0400

    popular movie bugs fixed

--- src/components/PopularMovies.vue
+++ src/components/PopularMovies.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="PopularMovies">
-  <section>
-    <button @click="goback">GoBack</button>
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Year</th>
-          <th>Rating</th>
-          <th>Rated</th>
+    <section>
+      <button @click="goback">GoBack</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Year</th>
+            <th>Imbd_ID</th>
+            <th>Rating</th>
+            <th>Rated</th>
+          </tr>
+        </thead>
+        <tr v-for="(x, pos) in allMovies" :key="pos">
+          <td>{{ x.title }}</td>
+          <td>{{ x.year }}</td>
+          <td>{{ x.imdb_id }}</td>
+          <td>{{ x.rating }}</td>
+          <td>{{ x.rated }}</td>
         </tr>
-      </thead>
-      <tr v-for="(x, pos) in allMovies" :prop="x" :key="pos">
-        <td>{{ x.title }}</td>
-        <td>{{ x.year }}</td>
-        <td>{{}}</td>
-        <td>{{}}</td>
-      </tr>
-      <tbody></tbody>
-    </table>
-  </section>
+        <tbody></tbody>
+      </table>
+    </section>
   </div>
 </template>
 
@@ -31,16 +33,13 @@ interface Movie {
   title: string;
   year: number;
   imdb_id: string;
-  rating: string;
+  rating: number;
   rated: string;
 }
 
 @Component
 export default class PopularMivies extends Vue {
-  private allMovies: any[] = [];
-  private movie_imbd: string[] = [];
-  private movie_rated: string[] = [];
-  private movie_rating: string[] = [];
+  private allMovies: Movie[] = [];
   mounted(): void {
     axios
       .get("https://movies-tvshows-data-imdb.p.rapidapi.com/", {
@@ -54,43 +53,38 @@ export default class PopularMivies extends Vue {
       .then((r: AxiosResponse) => r.data)
       .then((p: any) => p.movie_results)
       .then((movieInfo: Movie[]) => {
-        this.allMovies.push(...movieInfo);
-        this.movie_imbd = movieInfo.map((a) => a.imdb_id);
-        this.getpop();
+         this.allMovies = movieInfo;
+        for (let x = 0; x < this.allMovies.length; x++) {
+          const sth = this.getpop(this.allMovies[x].imdb_id)
+          .then((mmm: any[]) =>{
+            movieInfo[x].rating = mmm[0];
+            movieInfo[x].rated = mmm[1];
+          })
+          
+        }
       });
   }
   goback() {
     this.$router.back();
   }
-  getpop() {
-    //the maximum number for getting images of the movies is 15,
-    //I tried 20 but it returns a 429 error message because of the limit of the API
-    for (let x = 0; x < 15; x++) {
-      axios
-        .get("https://movies-tvshows-data-imdb.p.rapidapi.com/", {
-          params: {
-            type: "get-movie-details",
-            imdb: this.movie_imbd[x],
-          },
-          headers: {
-            "x-rapidapi-key":
-              "ae7d7525edmshe28bd6a6eef3638p14f263jsnb3486bf90e1b",
-            "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
-          },
-        })
-        .then((r: AxiosResponse) => r.data)
-        .then((p: any) => {
-          this.movie_rated.push(p.rated);
-          this.movie_rating.push(p.imdb_rating);
-        });
-    }
-        //console.log(this.movie_rated);
-        console.log(this.movie_rating);
-     for(let x = 0; x < 15; x++){
-         this.allMovies.push({rating : this.movie_rating[x]});
-     }
-     console.log(this.allMovies);
-    
+  async getpop(id: string) {
+    return await axios
+      .get("https://movies-tvshows-data-imdb.p.rapidapi.com/", {
+        params: {
+          type: "get-movie-details",
+          imdb: id,
+        },
+        headers: {
+          "x-rapidapi-key":
+            "ae7d7525edmshe28bd6a6eef3638p14f263jsnb3486bf90e1b",
+          "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+        },
+      })
+      .then((r: AxiosResponse) => r.data)
+      .then((p: any) => {
+       return [p.imdb_rating, p.rated];
+      });
+      
   }
 }
 </script>
